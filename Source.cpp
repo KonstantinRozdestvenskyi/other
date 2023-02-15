9e382e07fb9377b9c88104c9fd1368f65f7ea017
Костя Рождественский <kosta7497@gmail.com>
2023-02-15 13:11:25 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+struct Node_BST
+{
+    int data;
+    Node_BST* left;
+    Node_BST* right;
+    Node_BST* parent;
+};
+
+struct Node_RBT
+{
+    int data;
+    Node_RBT* left;
+    Node_RBT* right;
+    Node_RBT* parent;
+    char color;
+};
+
+struct Node_AVLT
+{
+    int data;
+    Node_AVLT* left;
+    Node_AVLT* right;
+    Node_AVLT* parent;
+};
